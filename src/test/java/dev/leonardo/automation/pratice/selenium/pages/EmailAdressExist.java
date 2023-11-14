53ad06c1cc95e88a0efcc8d5352616c5301e4029
lleonardx <lleonardx.alves@gmail.com>
2023-11-14 15:04:13 -0400

    upverificação de conta existente

--- src/test/java/dev/leonardo/automation/pratice/selenium/pages/EmailAdressExist.java
+++ src/test/java/dev/leonardo/automation/pratice/selenium/pages/EmailAdressExist.java
@@ -1,4 +1,32 @@
 package dev.leonardo.automation.pratice.selenium.pages;
 
-public class EmailAdressExist {
+import org.openqa.selenium.By;
+
+public class EmailAdressExist extends BasePage {
+	private By nameLocator = By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[2]") ;
+	private By emailAdressLocator = By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]");
+	private By signinUpLocator = By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/button");
+	private By alertEmailLocator = By.cssSelector("#form > div > div > div:nth-child(3) > div > form > p");
+	
+	public void insertNameToRegister() {
+		if(super.isDisplayed(nameLocator)) {
+			type("Java Teste", nameLocator);
+		}else {
+			System.out.println("name textbox was not present");
+		}
+	}
+	public void insertEmailToRegister() {
+		if(super.isDisplayed(emailAdressLocator)) {
+			type("javateste01@gmail.com", emailAdressLocator);
+			click(signinUpLocator);
+		}else {
+			System.out.println("email textbox was not present");
+		}
+	}
+	public String getAlertMessage() {
+		 super.waitVisibilityOfElementLocated(alertEmailLocator);
+		 return super.getText(alertEmailLocator);
+	 }
+	
+	
 }
--- src/test/java/dev/leonardo/automation/pratice/selenium/tests/EmailAdressExistTest.java
+++ src/test/java/dev/leonardo/automation/pratice/selenium/tests/EmailAdressExistTest.java
@@ -1,26 +1,34 @@
 package dev.leonardo.automation.pratice.selenium.tests;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import dev.leonardo.automation.pratice.selenium.pages.BasePage;
+import dev.leonardo.automation.pratice.selenium.pages.EmailAdressExist;
 
 class EmailAdressExistTest extends BasePage {
+	private EmailAdressExist emailAdressExist;
+	private final String URL = "https://automationexercise.com/login";
 
 	@BeforeEach
 	void setUp() throws Exception {
+		this.emailAdressExist = new EmailAdressExist();
+		this.emailAdressExist.visit(this.URL);
 	}
-
 	@AfterEach
 	void tearDown() throws Exception {
+		//this.emailAdressExist.quitWebDriver();
 	}
-
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		//when
+		this.emailAdressExist.insertNameToRegister();
+		this.emailAdressExist.insertEmailToRegister();
+		//then
+		String expected = "Email Address already exist!";
+		String actual = this.emailAdressExist.getAlertMessage();
+		Assertions.assertEquals(expected, actual);
+		
 	}
-
 }
